9c7fbe413d51140d2ce8891e366bcbbee73c5009
YukenNtsk <130311990+YukenNtsk@users.noreply.github.com>
2023-11-26 23:09:13 +0100

    Update score.js

--- javascript/score.js
+++ javascript/score.js
@@ -8,23 +8,32 @@ document.querySelector("h3#porciento").innerHTML = `${porciento}%`;
 
 document.querySelector("#circle").style.strokeDashoffset = 440 - (440 * porciento) / 100
 
-console.log(document.querySelector("#circle").style.dashoffset)
-
 console.log(porciento)
 
 window.onload = () => {
     if (porciento < 25) {
+        document.querySelector("#circle").style.sroke = "#00ff00"
         document.querySelector("div#text").innerHTML = `
             A ti no te gusta hablar con otras personas, \n
-            Tienes que hablar mas con tus amigos
+            Tienes que hablar más con tus amigos.
         `
     } else if (25 <= porciento && porciento < 50) {
+        document.querySelector("#circle").style.sroke = "#ffff00"
         document.querySelector("div#text").innerHTML = `
-            No eres un cotillo, pero te gusta conocer un poco a los demas
+            No eres un cotillo, pero te gusta conocer un poco a los demás.
         `
-    } else if (50 <= porciento) {
+    } else if (50 <= porciento && porciento < 75) {
+        document.querySelector("#circle").style.sroke = "#99aa00"
         document.querySelector("div#text").innerHTML = `
-            Eres un cotillo!
+            Eres un cotillo! Te gusta mucho hablar con personas\n
+            y trabar amistad.
+        `
+    } else if (75 <= porciento) {
+        document.querySelector("#circle").style.sroke = "#ff0000"
+        document.querySelector("div#text").innerHTML = `
+            Eres un cotillo! Te gusta muchissimo hablar con personas\n
+            pero tienes que respetar la privacidad de las personas\n
+            un poco más...
         `
     }
 }
