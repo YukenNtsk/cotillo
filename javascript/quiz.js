9ddee8a163ba52e4646128fb8096cb43d52bea5b
YukenNtsk <130311990+YukenNtsk@users.noreply.github.com>
2023-11-26 16:00:46 +0100

    Add files via upload

---
+++ javascript/quiz.js
@@ -0,0 +1,120 @@
+let preguntas = [
+    {
+        id: 1,
+        pregunta: "En el autobus, una pareja a tu lado empeza conversar",
+        respuestas: {
+            "Intentas no escucharles": 0,
+            "No dices nada pero escucha a la conversación": 1,
+            "Empiezas hablar con ellos": 2,
+        }
+    },
+    {
+        id: 2,
+        pregunta: "Tu amigo/a te cuenta un secreto muy divertido/interesante",
+        respuestas: {
+            "Lo dices a un amigo en quién confías": 1,
+            "Le compartes a todo el mundo, pero no dices de quien es": 2,
+            "No le compartes a nadie": 0,
+        }
+    },
+    {
+        id: 3,
+        pregunta: "Tu amigo/a te envia un mensaje",
+        respuestas: {
+            "Respondes con un mensaje": 1,
+            "Le llamas": 3,
+            "Respondes con un mensaje de voz": 2
+        }
+    },
+    {
+        id: 4,
+        pregunta: "Tu amigo/a esta escribiendo un mensaje en su telefono y parece muy divertido",
+        respuestas: {
+            "No haces nada": 0,
+            "Lees los mensajes sobre su hombro": 2,
+            "Preguntas con quién está hablando": 1,
+        }
+    },
+    {
+        id: 5,
+        pregunta: "Encontras un vecino en tu edificio o al lado de tu casa",
+        respuestas: {
+            "Le dices hola, pero las conversacion se termina ahí": 1,
+            "No dices nada si el vecino no te reconoce": 0,
+            "Le dices hola y le compartes un rumor que circula en el barrio": 2,
+        }
+    },
+    {
+        id: 6,
+        pregunta: "Alguien deja su teléfono desbloqueado",
+        respuestas: {
+            "Abres una aplicación y lees los mensajes recientes": 2,
+            "Apaga el teléfono": 0,
+            "Miras a las aplicaciónes pero no hace nada": 1,
+        }
+    },
+    {
+        id: 7,
+        pregunta: "Tu amigo/a deja su teléfono y alguien le llama",
+        respuestas: {
+            "Observas quién está llamando": 1,
+            "Llamas el amigo para decirlo que alguien está llamando": 0,
+            "Coges el teléfono": 2,
+        }
+    },
+]
+
+var numero = 1
+
+var puntos = 0
+
+window.onload = function () {
+    show(numero)
+}
+
+function show (numero) {
+    let pregunta = document.getElementById("pregunta")
+    let respuestas = preguntas[numero-1].respuestas
+    pregunta.innerHTML = `
+        <h1>Pregunta ${numero}:</h1>
+        <h3>${preguntas[numero-1].pregunta}</h3>
+        <div class="respuesta">
+            <li class="btn">${Object.keys(respuestas)[0]}</li>
+            <li class="btn">${Object.keys(respuestas)[1]}</li>
+            <li class="btn">${Object.keys(respuestas)[2]}</li>
+        </div>
+    `
+    toggleActive()
+}
+
+function toggleActive(){
+    let option = document.querySelectorAll("li.btn");
+    for(let i=0; i < option.length; i++){
+        option[i].onclick = function(){
+            for(let i=0; i < option.length; i++){
+                if(option[i].classList.contains("active")){
+                    option[i].classList.remove("active");
+                }
+            }
+            option[i].classList.add("active");
+        }
+    }
+}
+
+function next () {
+    if (numero == preguntas.length) {
+        location.href = "resultado.html"
+        console.log(puntos)
+    }
+    let user_respuesta = document.querySelector("li.btn.active").innerHTML;
+    if (!user_respuesta) {
+        return
+    } else {
+        console.log(preguntas[numero-1].respuestas[user_respuesta])
+        puntos += preguntas[numero-1].respuestas[user_respuesta]
+        sessionStorage.setItem("puntos", puntos)
+    }
+    numero ++
+    console.log(puntos)
+    show(numero)
+}
---
+++ javascript/score.js
@@ -0,0 +1,30 @@
+let puntos = sessionStorage.getItem("puntos");
+
+let porciento = Math.round((puntos/15)*100)
+
+console.log(porciento)
+
+document.querySelector("h3#porciento").innerHTML = `${porciento}%`;
+
+document.querySelector("#circle").style.strokeDashoffset = 440 - (440 * porciento) / 100
+
+console.log(document.querySelector("#circle").style.dashoffset)
+
+console.log(porciento)
+
+window.onload = () => {
+    if (porciento < 25) {
+        document.querySelector("div#text").innerHTML = `
+            A ti no te gusta hablar con otras personas, \n
+            Tienes que hablar mas con tus amigos
+        `
+    } else if (25 <= porciento && porciento < 50) {
+        document.querySelector("div#text").innerHTML = `
+            No eres un cotillo, pero te gusta conocer un poco a los demas
+        `
+    } else if (50 <= porciento) {
+        document.querySelector("div#text").innerHTML = `
+            Eres un cotillo!
+        `
+    }
+}
