f5771c70414fa99106f275d9dbd2a2e0691d4a1f
YukenNtsk <130311990+YukenNtsk@users.noreply.github.com>
2023-11-27 10:55:58 +0100

    Update score.js

--- javascript/score.js
+++ javascript/score.js
@@ -14,25 +14,28 @@ window.onload = () => {
     if (porciento < 25) {
         document.querySelector("#circle").style.stroke = "#00ff00"
         document.querySelector("div#text").innerHTML = `
-            A ti no te gusta hablar con otras personas, \n
-            Tienes que hablar más con tus amigos.
+            A ti no te gusta hablar con otras personas.\n
+            Tienes que hablar más con tus amigos y volverte\n
+            más sociable
         `
     } else if (25 <= porciento && porciento < 50) {
         document.querySelector("#circle").style.stroke = "#ffff00"
         document.querySelector("div#text").innerHTML = `
-            No eres un cotillo, pero te gusta conocer un poco a los demás.
+            No eres chismoso/a, pero te gusta conocer un poco a los demás.\n
+            Eres una persona de confianza
         `
     } else if (50 <= porciento && porciento < 75) {
         document.querySelector("#circle").style.stroke = "#ffaa00"
         document.querySelector("div#text").innerHTML = `
-            Eres un cotillo! Te gusta mucho hablar con personas\n
-            y trabar amistad.
+            Eres chismoso/a! Te gusta mucho hablar con personas\n
+            y trabar amistad. Pero probablemente a las personas\n
+            les gusta hablar contigo también
         `
     } else if (75 <= porciento) {
         document.querySelector("#circle").style.stroke = "#ff0000"
         document.querySelector("div#text").innerHTML = `
-            Eres un cotillo! Te gusta muchissimo hablar con personas\n
-            pero tienes que respetar la privacidad de las personas\n
+            Eres muy chismoso/a! Te gusta muchissimo hablar con personas\n
+            pero tienes que respetar la privacidad de los demás\n
             un poco más...
         `
     }
