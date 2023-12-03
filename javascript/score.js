7486de7dd9d44f36be0c53fe0e28423c41b458d5
YukenNtsk <130311990+YukenNtsk@users.noreply.github.com>
2023-12-03 21:30:33 +0100

    Update score.js

--- javascript/score.js
+++ javascript/score.js
@@ -14,7 +14,7 @@ window.onload = () => {
     if (porciento < 25) {
         document.querySelector("#circle").style.stroke = "#00ff00"
         document.querySelector("div#text").innerHTML = `
-            A ti no te gusta chsimorrear para nada.\n
+            A ti no te gusta chismorrear para nada.\n
             Tienes que hablar más con tus amigos y volverte\n
             más sociable...
         `
@@ -22,19 +22,19 @@ window.onload = () => {
         document.querySelector("#circle").style.stroke = "#ffff00"
         document.querySelector("div#text").innerHTML = `
             No eres chismoso/a, pero te gusta conocer un poco a los demás.\n
-            Eres una persona de confianza!
+            ¡Eres una persona de confianza!
         `
     } else if (50 <= porciento && porciento < 75) {
         document.querySelector("#circle").style.stroke = "#ffaa00"
         document.querySelector("div#text").innerHTML = `
-            Eres chismoso/a! Te gusta mucho hablar con personas\n
+            ¡Eres chismoso/a! Te gusta mucho hablar con personas\n
             y trabar amistad. Pero probablemente a las personas\n
             les gusta hablar contigo también.
         `
     } else if (75 <= porciento) {
         document.querySelector("#circle").style.stroke = "#ff0000"
         document.querySelector("div#text").innerHTML = `
-            Eres muy chismoso/a! Te gusta muchísimo hablar con personas\n
+            ¡Eres muy chismoso/a! Te gusta muchísimo hablar con personas\n
             pero tienes que respetar la privacidad de los demás\n
             un poco más...
         `
